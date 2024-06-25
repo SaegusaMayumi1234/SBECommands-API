3eb4b1fcc0ecb0e02ce135b86240b146bbefe99c
Theresano Julian Laksono <theresano.laksono@binus.edu>
2024-06-25 10:39:02 +0700

    feat: Initial commit

---
+++ src/index.ts
@@ -0,0 +1,47 @@
+import express, { Express } from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import './utils/uncaughtError';
+import config from './config/config';
+import * as logger from './utils/logger';
+// import db from './storages/mongoDB/index';
+// import NotFound from './middlewares/notFound';
+// import ErrorHandler from './middlewares/errorHandler';
+// import RoutesV1 from './routes/v1';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const app: Express = express();
+
+(async () => {
+  //   await db.mongoose.connect(config.mongodb.uri, {
+  //     dbName: config.mongodb.name,
+  //   });
+
+  app.set('trust proxy', config.proxied);
+
+  app.use(
+    helmet({
+      contentSecurityPolicy: false,
+    }),
+  );
+
+  app.use(express.json({ limit: '15mb' }));
+  app.use(express.urlencoded({ extended: true }));
+
+  app.use(cors());
+
+  app.use(express.static(path.join(__dirname, '/public')));
+
+  //   app.use('/v1', RoutesV1);
+
+  //   app.use(NotFound);
+  //   app.use(ErrorHandler);
+
+  app.listen(config.port, () => {
+    logger.info(`SBECommands-API server is running at http://localhost:${config.port}`);
+  });
+})();
---
+++ src/storage/local/README.md
@@ -0,0 +1,1 @@
+Any file in this directory will be created automatically!
