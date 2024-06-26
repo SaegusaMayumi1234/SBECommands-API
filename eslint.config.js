bdfe68942b4dcfe258f8910f938b1604b48d6681
SaegusaMayumi1234 <theresanojulianl1234@gmail.com>
2024-06-27 01:27:27 +0700

    fix: Changed eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -8,5 +8,6 @@ export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.r
   ignores: ['.dist/'],
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
   },
 });
